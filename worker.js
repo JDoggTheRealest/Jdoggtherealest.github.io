40c4e20f9082cd37a21b3b7deb26c44fe1f771d1
JDoggTheRealest <43619041+JDoggTheRealest@users.noreply.github.com>
2019-01-26 15:34:43 +0100

    v1.0.3

--- worker.js
+++ worker.js
@@ -1,18 +1,12 @@
-//Lager en funksjon som oppdateres hvert 10ms
 function timedCount() {
 	
-	//Sender message til hovedkoden
-	postMessage("crap");
-	
-	//Legger til timeout
-  	setTimeout("timedCount()",10);
+  postMessage("crap");
+  setTimeout("timedCount()",10);
 	
 }
 
-//Sjekker om sent melding
 onmessage = function() {
-	
-	//Starter funksjons "loopen"
+
 	timedCount();
 	
 }
