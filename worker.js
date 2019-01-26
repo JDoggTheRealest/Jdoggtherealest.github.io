f709ef91f03526e683ca48ef87057e475c3d8ad2
JDoggTheRealest <43619041+JDoggTheRealest@users.noreply.github.com>
2019-01-26 20:24:00 +0100

    v2.0.0
    Added: 
    - Play and pause buttons
    - Forward and backwards buttons
    - Fixed time calculation
    - Tweaked some ui
    - Added support for more characters in title, 
      now it also update size based on the length of the string

--- worker.js
+++ worker.js
@@ -1,12 +1,31 @@
+//Lagrer en tom interval
+var timeout = setInterval("");
+
+//Lager en funksjon for å oppdatere klokken
 function timedCount() {
 	
-  postMessage("crap");
-  setTimeout("timedCount()",10);
+	//Sender melding til hovedvindu
+	postMessage("crap");
 	
 }
 
-onmessage = function() {
+//Sjekker om mottat melding
+onmessage = function(e) {
 
-	timedCount();
+	//Sjekker om skal stoppe
+	if(e.data == "stop") {
+		
+		//Fjerner intervalet
+		clearInterval(timeout);
+		
+	} else if(e.data == "crap") { //Sjekker om meldingen sendt er "crap"
+		
+		//Starter oppdateringer
+		timedCount();
+		
+		//Gjør slik at oppdaterer hvert 20ms
+		timeout = setInterval("timedCount()",20);
+		
+	}
 	
 }
